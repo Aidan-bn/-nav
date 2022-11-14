94d9343c8f8b90a32821008fc99b8e648272da16
Aidan - bn <81457065+Aidan-bn@users.noreply.github.com>
2022-11-14 16:18:13 +0300

    Update index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const moja = document.querySelector('.img-hamburger');
+const hiddenDiv = document.querySelector('.mob-overlay');
+
+moja.addEventListener('click', () => {
+
+    hiddenDiv.style.display = 'flex';
+    
+});
