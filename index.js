418025a275adc9428c542c50adbc74fdadf2b099
Aidan - bn <81457065+Aidan-bn@users.noreply.github.com>
2022-11-14 16:34:35 +0300

    Add linters.yml

--- index.js
+++ index.js
@@ -2,7 +2,5 @@ const moja = document.querySelector('.img-hamburger');
 const hiddenDiv = document.querySelector('.mob-overlay');
 
 moja.addEventListener('click', () => {
-
     hiddenDiv.style.display = 'flex';
-    
 });
